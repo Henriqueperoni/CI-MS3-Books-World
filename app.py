9e583233ba18fe7a1eb74d062a6cc01a9d99cf2c
Henriqueperoni <henriqueperoni@hotmail.com>
2020-11-25 09:07:58 +0000

    Create login template

--- app.py
+++ app.py
@@ -48,6 +48,11 @@ def sign_up():
     return render_template("sign_up.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
