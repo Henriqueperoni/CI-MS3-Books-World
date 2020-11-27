75c2022ec11ccc3416fe7061c6ed0e3c1b1c181d
Henriqueperoni <henriqueperoni@hotmail.com>
2020-11-27 09:27:09 +0000

    Add JavaScript funcionality to add book modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
-  });
+});
+
+function startModal() {
+    const modal = document.querySelector(".container-modal")
+    modal.classList.add("show-modal");
+
+    modal.addEventListener("click", (e) => {
+        if(e.target.id == "modal-review" || e.target.className == "close-modal") {
+            modal.classList.remove("show-modal")
+        }
+    })
+}
+
+const addBook = document.getElementById("add-book")
+addBook.addEventListener("click", () => {
+    startModal()
+})
+console.log(addBook)
+
+
+
