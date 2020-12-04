42de37c84f42b9aa2746b0b540afe8702aa4519d
Henriqueperoni <henriqueperoni@hotmail.com>
2020-12-04 09:05:41 +0000

    Fix the dynamically adder input field and style it

--- static/js/script.js
+++ static/js/script.js
@@ -22,9 +22,11 @@ addBook.addEventListener("click", () => {
 })
 
 const editBook = document.getElementById("edit-book")
-editBook.addEventListener("click", () => {
-    startModalBooks()
-})
+if(editBook){
+     editBook.addEventListener("click", () => {
+         startModalBooks()
+     })
+}
 
 function deleteModal() {
     const modal = document.querySelector(".container-modal-delete")
@@ -37,23 +39,29 @@ function deleteModal() {
     })
 }
 
-
 const deleteBook = document.getElementById("delete-book")
+if(deleteBook) {
 deleteBook.addEventListener("click", () => {
     deleteModal()
 })
+}
 
+
+
+// Function to dynamically add input fields
+//https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
 $(document).ready(function() {
 	var max_fields      = 10; //maximum input boxes allowed
 	var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
-	var add_button      = $(".add_field_button"); //Add button ID
+    var add_button      = $(".add-field-button"); //Add button ID
 	
 	var x = 1; //initlal text box count
-	$(add_button).click(function(e){ //on add input button click
+    $(add_button).click(function(e){ //on add input button click
+        console.log('clicked')
 		e.preventDefault();
 		if(x < max_fields){ //max input box allowed
 			x++; //text box increment
-			$(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+			$(wrapper).append('<div><label for="img_url">Insert image URL</label><input id="img_url" name="img_url" type="text" class="validate" minlength="5" maxlength="100" required/><a href="#" class="remove_field btn-small red darken-2">Remove</a></div>'); //add input box
 		}
 	});
 	
