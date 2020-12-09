9f9c418be0023dc433e6f22ab0b68757452c036b
Henriqueperoni <henriqueperoni@hotmail.com>
2020-12-09 21:28:44 +0000

    Fix bug that was duplicating all the fields on the form inside the edit modal

--- static/js/script.js
+++ static/js/script.js
@@ -52,21 +52,23 @@ deleteBook.addEventListener("click", () => {
 // Function to dynamically add input fields
 //https://www.sanwebe.com/2013/03/addremove-input-fields-dynamically-with-jquery
 $(document).ready(function() {
-	var max_fields      = 10; //maximum input boxes allowed
-	var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
-    var add_button      = $(".add-field-button"); //Add button ID
+	let max_fields      = 10; //maximum input boxes allowed
+	let wrapper   		= $(".input_fields_wrap"); //Fields wrapper
+    let add_button      = $(".add-field-button"); //Add button ID
+    let inputs          = $(".container-inputs")
 	
-	var x = 1; //initlal text box count
+	let x = 1; //initlal text box count
     $(add_button).click(function(e){ //on add input button click
         console.log('clicked')
 		e.preventDefault();
 		if(x < max_fields){ //max input box allowed
 			x++; //text box increment
-			$(wrapper).append('<div><label for="img_url">Insert image URL</label><input id="img_url" name="img_url" type="text" class="validate" minlength="5" maxlength="100" required/><a href="#" class="remove_field btn-small red darken-2">Remove</a></div>'); //add input box
-		}
+            $(inputs).append('<div class="input-field col s12 input_fields_wrap"><label for="img_url">Insert image URL</label><input id="img_url" name="img_url" type="text" class="validate" minlength="5" maxlength="100" required/><a href="#" class="remove_field btn-small red darken-2">Remove</a></div>'); //add input box
+            
+        }
 	});
 	
-	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+	$(inputs).on("click",".remove_field", function(e){ //user click on remove text
 		e.preventDefault(); $(this).parent('div').remove(); x--;
 	})
 });
