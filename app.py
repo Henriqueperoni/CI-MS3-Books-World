b778fced3853621251acac7425106a8df505bcd7
Henriqueperoni <henriqueperoni@hotmail.com>
2020-12-21 11:35:29 +0000

    Fix routes and Done button

--- app.py
+++ app.py
@@ -230,10 +230,11 @@ def add_book_in_list(list_name):
     return redirect(url_for("view_list", list_name=book_list["_id"]))
 
 
-@app.route("/book_info/<book_name>")
-def book_info(book_name):
+@app.route("/book_info/<list_name>/<book_name>")
+def book_info(list_name, book_name):
     book = mongo.db.books_in_list.find_one({"_id": ObjectId(book_name)})
-    return render_template("book_info.html", book=book)
+    book_list = mongo.db.book_lists.find_one({"_id": ObjectId(list_name)})
+    return render_template("book_info.html", book=book, list=book_list)
 
 
 @app.route("/discover")
